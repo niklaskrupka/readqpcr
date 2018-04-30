ce76908ca007ce219e1a757bb11546ca6391a485
Niklas Krupka <niklas.krupka@gmail.com>
2018-04-29 22:36:51 -0400

    Had to fix a few things after R CMD check

--- R/plot_qpcr.R
+++ R/plot_qpcr.R
@@ -1,20 +1,34 @@
+#' @title Plot normalized qPCR data in a nice format
+#' @description Takes normalized qPCR data and turns it into a ggplot2 object for visualization
+#' @param the_data A data frame containing normalized qPCR data. If read_qpcr is used this would be the second
+#'   element of the returned list. Make sure the data frame contains at least the columns "Sample" and "Genotype".
+#' @param norm Reference gene to use
+#' @return A ggplot2 object
+#' @examples
+#' \dontrun{
+#' plot_qpcr(my_pcrdata[[2]], norm = Gapdh)
+#' }
+#' @import magrittr
+#' @export
 plot_qpcr <- function(the_data,
                       norm){
   # Unquote the normalization factor, which is provided so it isn't plotted.
-  norm <- enquo(norm)
+  norm <- dplyr::enquo(norm)
   # Select Columns that contain calculated values, remove normalization gene and plot.
   the_data %>%
-    select(Sample, Genotype, contains("2^-dCt"), -contains(quo_name(norm))) %>%
-    gather(-Sample, -Genotype, key = "Gene", value = "Relative expression") %>%
-    separate(col = Gene, into = c("Gene", "Method"), sep = "_") %>%
-    ggplot(aes(x = Genotype, y = `Relative expression`, color = Genotype)) +
-    theme_minimal() +
-    theme(axis.title.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          axis.text.x  = element_blank()) +
-    scale_colour_manual(values = c("#333333", "#d55e00")) +
-    stat_summary(geom = "bar", fun.y = "mean", fill = NA, width = 0.8) +
-    stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0.2) +
-    geom_jitter(width = 0.2, size = 2) +
-    facet_wrap(~Gene, scales = "free_y")
+    dplyr::select(Sample, Genotype,
+                  dplyr::contains("2^-dCt"),
+                  -dplyr::contains(dplyr::quo_name(norm))) %>%
+    tidyr::gather(-Sample, -Genotype, key = "Gene", value = "Relative expression") %>%
+    tidyr::separate(col = Gene, into = c("Gene", "Method"), sep = "_") %>%
+    ggplot2::ggplot(ggplot2::aes(x = Genotype, y = `Relative expression`, color = Genotype)) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(axis.title.x = ggplot2::element_blank(),
+                   axis.ticks.x = ggplot2::element_blank(),
+                   axis.text.x  = ggplot2::element_blank()) +
+    ggplot2::scale_colour_manual(values = c("#333333", "#d55e00")) +
+    ggplot2::stat_summary(geom = "bar", fun.y = "mean", fill = NA, width = 0.8) +
+    ggplot2::stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0.2) +
+    ggplot2::geom_jitter(width = 0.2, size = 2) +
+    ggplot2::facet_wrap(~Gene, scales = "free_y")
 }
--- R/read_qpcr.R
+++ R/read_qpcr.R
@@ -1,4 +1,22 @@
-read_qpr <- function(datafolder,
+#' @title Read and process qPCR data
+#' @description Reads an exported Bio-Rad qPCR result file, calculates quality
+#'   controls summaries and performs 2^-ddCt calculations.
+#' @param datafolder The directory, which contains one or multiple Biorad
+#'   CSVs. Important: all CSV files in that location will be read and cause
+#'   problems if they are not in the right format!
+#' @param metadatafile A CSV file that contains sample metadata. At least a
+#'   "Sample" and"Genotype" field have to be in that file
+#' @param norm Reference gene to use
+#' @return A list of two data frames: The first one contains the data
+#'   collapsed by technical replicates and contains quality control indicators.
+#'   The second one contains the 2^-ddCt calculations.
+#' @examples
+#' \dontrun{
+#' read_qpcr("./raw_data/", metadatafile = "./metadata.csv", norm = Gapdh)
+#' }
+#' @import magrittr
+#' @export
+read_qpcr <- function(datafolder,
                      metadatafile = "./metadata.csv",
                      norm){
   # Turn off the readr warnings
@@ -7,44 +25,45 @@ read_qpr <- function(datafolder,
   bhead <- c("File", "Well", "Target", "Content", "Replicate", "Sample", "Cq")
   # Check if norm is set and unquote it
   if(missing(norm)){stop("Parameter norm is missing. You need to specify a housekeeping gene")}
-  norm <- enquo(norm)
+  norm <- dplyr::enquo(norm)
   # Read the Bio-Rad CSVs. The first 19 rows in a Bio-Rad CSV export are the header, ignore them
   message("Loading the Bio-Rad CSV files.")
-  qpcr_data <- lapply(list.files(path = datafolder, full.names = TRUE, pattern = "\\.csv$"), read_csv, skip = 19)
-  qpcr_data <- bind_rows(qpcr_data, .id = "File")
+  qpcr_data <- lapply(list.files(path = datafolder, full.names = TRUE, pattern = "\\.csv$"),
+                      readr::read_csv, skip = 19)
+  qpcr_data <- dplyr::bind_rows(qpcr_data, .id = "File")
   # Make sure it's the right format and that there is actually the specified calibrator
   if(!all(names(qpcr_data) == bhead)){
     stop("The data files need to have the format: ", bhead)
     }
-  if(!(quo_name(norm) %in% qpcr_data$Target)){
+  if(!(dplyr::quo_name(norm) %in% qpcr_data$Target)){
     stop("The specified calibrator does not exist.")
   }
   # Read the Metadata and make sure it's the right format.
   # It has to contain at least the columns Sample and Genotype
   message("Loading the Metadata.")
-  metadata <- read_csv(metadatafile)
+  metadata <- readr::read_csv(metadatafile)
   if(!all(c("Sample", "Genotype") %in% names(metadata))){
     stop("The metadata file needs to contain at least a Sample and Genotype column.")
   }
   # Collapse technical replicates and calculate statistics.
   message("Collapsing technical replicates and calculating statistics.")
   qpcr_data_collapsed <- qpcr_data %>%
-    group_by(Target, Sample, Content) %>%
-    summarize(Cq_mean = mean(Cq),
-              Technical_replicates = n(),
-              Spread = max(Cq) - min(Cq),
-              SD = sd(Cq)) %>%
-    ungroup()
+    dplyr::group_by(Target, Sample, Content) %>%
+    dplyr::summarize(Cq_mean = mean(Cq),
+                     Technical_replicates = dplyr::n(),
+                     Spread = max(Cq) - min(Cq),
+                     SD = stats::sd(Cq)) %>%
+    dplyr::ungroup()
   # Do the 2^-dCT calculations
   message("Perform 2^dCT calculations.")
   qpcr_data_calculated <- qpcr_data_collapsed %>%
-    select(Sample, Target, Cq_mean) %>%
+    dplyr::select(Sample, Target, Cq_mean) %>%
     # Remove rows without a sample name (these are most likely NTCs)
-    filter(!is.na(Sample)) %>%
-    spread(key = "Target", value = "Cq_mean") %>%
-    mutate_if(is.double, funs(dCt      = . - !!norm,
+    dplyr::filter(!is.na(Sample)) %>%
+    tidyr::spread(key = "Target", value = "Cq_mean") %>%
+    dplyr::mutate_if(is.double, dplyr::funs(dCt      = . - !!norm,
                               `2^-dCt` = 2 ^(!!norm - .))) %>%
-    left_join(metadata, by = "Sample")
+    dplyr::left_join(metadata, by = "Sample")
   # Return the two tibbles
   message("Finished.")
   return(list(qpcr_data_collapsed, qpcr_data_calculated))
